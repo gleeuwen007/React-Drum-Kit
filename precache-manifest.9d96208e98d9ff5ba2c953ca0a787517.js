7d3aa7c2b76b6249b71cc6effca2cd418303b102
Greg Leeuwen <gleeuwen@gregleewensimac.home>
2020-07-27 10:06:39 -0400

    Updates

---
+++ precache-manifest.9d96208e98d9ff5ba2c953ca0a787517.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a401906004c769deb08ff699f828e32",
+    "url": "/React-Drum-Kit/index.html"
+  },
+  {
+    "revision": "dfa7d4f85b3c9740a7cd",
+    "url": "/React-Drum-Kit/static/css/main.50d0aa39.chunk.css"
+  },
+  {
+    "revision": "0bd376d3b45764d69cc3",
+    "url": "/React-Drum-Kit/static/js/2.92decc71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Drum-Kit/static/js/2.92decc71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa7d4f85b3c9740a7cd",
+    "url": "/React-Drum-Kit/static/js/main.bcc51c7f.chunk.js"
+  },
+  {
+    "revision": "5d9041e502efc75b7a12",
+    "url": "/React-Drum-Kit/static/js/runtime-main.4036abd7.js"
+  },
+  {
+    "revision": "fbcefb3d6c61c600c49dd86c6247fd9f",
+    "url": "/React-Drum-Kit/static/media/b9r5sEL.fbcefb3d.jpg"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/React-Drum-Kit/static/media/boom.550be808.wav"
+  },
+  {
+    "revision": "a6e86dd7c2d3dee1ec0ad70c0d69df36",
+    "url": "/React-Drum-Kit/static/media/clap.a6e86dd7.wav"
+  },
+  {
+    "revision": "4ba39e07d1f8d98b362b9b65d6544538",
+    "url": "/React-Drum-Kit/static/media/hihat.4ba39e07.wav"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/React-Drum-Kit/static/media/kick.4dd58595.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/React-Drum-Kit/static/media/openhat.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/React-Drum-Kit/static/media/ride.b0d70ec5.wav"
+  },
+  {
+    "revision": "80c4129b4b6b8810a079d6c311bf1d4b",
+    "url": "/React-Drum-Kit/static/media/snare.80c4129b.wav"
+  },
+  {
+    "revision": "2a6bb6f58bb711ed7ea121b225dcc92d",
+    "url": "/React-Drum-Kit/static/media/tink.2a6bb6f5.wav"
+  },
+  {
+    "revision": "572ed39d3a51fff664d47d7b7b508292",
+    "url": "/React-Drum-Kit/static/media/tom.572ed39d.wav"
+  }
+]);
